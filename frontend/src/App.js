50978223f24da92fc6b0390c0bb359493df06484
Kemoni <58960485+KemoniCodes@users.noreply.github.com>
2020-10-21 15:23:50 -0700

    Header + Footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 
 const App = () => {
   return (
-    <div className="App">
-      <h1>welcome to ALMV</h1>
-    </div>
+    <>
+      <Header />
+      <main>
+        <h1>welcome to ALMV</h1>
+      </main>
+      <Footer />
+    </>
   );
 }
 
---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <Footer>
+            footer
+        </Footer>
+    )
+}
+
+export default Footer
