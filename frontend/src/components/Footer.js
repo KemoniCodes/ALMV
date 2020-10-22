40e56878cd3522a420943698e065b7fb2d0ebb48
Kemoni <58960485+KemoniCodes@users.noreply.github.com>
2020-10-21 17:27:37 -0700

    footer styling

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from 'react';
+import '../scss/footer.scss';
 
 const Footer = () => {
     return (
         <footer>
-            footer
+            <ul>
+                <ul className='sub-list'>
+                    <li className='sub-header-text'>Contact</li>
+                    <li>hello@kemoniwilliams.tech</li>
+                    <li>@kwintech</li>
+                </ul>
+
+                <ul className='sub-list'>
+                    <li className='sub-header-text'>Privacy</li>
+                    <li>Privacy Policy</li>
+                    <li>Copyright</li>
+                </ul>
+
+                <ul className='sub-list'>
+                    <li className='sub-header-text'>Menu</li>
+                    <li>Shop</li>
+                    <li>About Us</li>
+                    <li>Editorial</li>
+                </ul>
+            </ul>
+            
+            <li className="sub-header-text">
+                COPYRIGHT© 2020 ALMV
+                </li>
         </footer>
     )
 }
