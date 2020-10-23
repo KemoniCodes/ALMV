d28337d1e78e75713c8a5809d2977d99394870fe
Kemoni <58960485+KemoniCodes@users.noreply.github.com>
2020-10-22 18:27:52 -0700

    route to link page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from '../src/components/Header';
 import Footer from '../src/components/Footer';
-import Landing from './layouts/Landing';
+import Landing from './screens/Landing';
 import './scss/app.scss';
+import ProductScreen from './screens/ProductScreen';
 
 
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
-      <Landing />
+      <Route exact path='/' component={Landing} />
+      <Route path='/product/:name/:id' component={ProductScreen} />
       <Footer />
-    </>
+    </Router>
   );
 }
 
---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProductScreen = () => {
+    return (
+        <div className='product'>
+            productz baby
+        </div>
+    )
+}
+
+export default ProductScreen
